1707e6863c463303c08200dec2a339509a8bd234
manik <maniksarker652@gmail.com>
2025-10-04 10:19:28 +0600

    make changes in notificaiton model

--- src/app/modules/notificationSetting/notificationSetting.model.ts
+++ src/app/modules/notificationSetting/notificationSetting.model.ts
@@ -1,16 +1,27 @@
-import { model, Schema } from "mongoose";
-import { INotificationSetting } from "./notificationSetting.interface";
+import { model, Schema } from 'mongoose';
+import { INotificationSetting } from './notificationSetting.interface';
 
-const notificationSettingSchema = new Schema<INotificationSetting>({
-    user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-    name: { type: String, required: true },
-    phone: { type: String },
-    email: { type: String, required: true, unique: true },
-    address: { type: String },
-    profile_image: { type: String, default: "" },
-    totalAmount: { type: Number, default: 0 },
-    totalPoint: { type: Number, default: 0 }
-}, { timestamps: true });
+const notificationSettingSchema = new Schema<INotificationSetting>(
+    {
+        user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+        generalNotification: {
+            type: Boolean,
+            default: true,
+        },
+        matchNotification: {
+            type: Boolean,
+            default: true,
+        },
+        messageNotification: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
 
-const notificationSettingModel = model<INotificationSetting>("NotificationSetting", notificationSettingSchema);
+const notificationSettingModel = model<INotificationSetting>(
+    'NotificationSetting',
+    notificationSettingSchema
+);
 export default notificationSettingModel;
